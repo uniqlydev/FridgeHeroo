8e87afae93ec2ebb0ccdaa26fb5c1196b100bbd8
Brendan Castillo <bomber8184@icloud.com>
2024-04-17 19:28:44 +0800

    Storing AI response to JSON file.

--- controllers/GenerativeAI.js
+++ controllers/GenerativeAI.js
@@ -6,32 +6,36 @@ require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.API_AI_STUDIO);
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-
 exports.sendQuery = async (req, res) => {
     try {
-
         // Retrieve fridge data 
         const fridge = await UserFridgeController.retrieveUserFridge(req, res);
-
-        // Make fridge into a json 
         const fridgeJson = JSON.stringify(fridge);
-
 
         const { prompt } = req.body;
 
-        const result = await model.generateContent(prompt + `Base your answer with my fridge data ${fridgeJson}` + "Your output structure should be \nIngredients:\nWhat I have\nWhat I should buy\nRecipe name:\nInstructions\n NOTE: You can suggest meals that goes beyond the available ingredients, just put it in the what i dont have");
+        const result = await model.generateContent(prompt + ` Base your answer with my fridge data ${fridgeJson}` + "Your output structure should be a json file consisting of recipe name, ingredients, required ingredients, available ingredients from the user fridge, and instructions. You are to provide a json file.\n Clean the json file make sure it doesn't have 1/2 cup or somehthing similar. If it has, 1/2 cup just make it 0.5 so it remains its data type. FOLLOW THIS STRICTLY if it's a NUMBER or a UNIT OF MEASUREMENT or it's A NUMBER OR DECIMAL. Don't add cup to it or teaspoons or something similar as it ruins the integrity of the json file.");
         const response = await result.response;
-        const text = response.text();
-    
-        const genResponse = new genresponse(prompt, text);
-    
-        res.status(200).send(genResponse);
-    }catch(e) {
+        const jsonResponseString = response.text();
+
+        // Remove Markdown-style code block notation
+        const cleanedJsonResponseString = jsonResponseString.replace(/```json\n|```/g, '');
+
+        // Parse the cleaned JSON string
+        const jsonResponse = JSON.parse(cleanedJsonResponseString);
+
+        // Sending JSON response
+        res.status(200).json(jsonResponse);
+    } catch (e) {
         res.status(500).send(e.message);
     }
 }
 
 exports.generateCartRecommendations = async (req,res) => {
-    
+    try {
+
+    }catch(e) {
+
+    }
 }
 
