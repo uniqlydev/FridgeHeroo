c6cab7a6d4c7f4d87549dd06571b56ae16048707
Brendan Castillo <bomber8184@icloud.com>
2024-04-17 09:34:08 +0800

    Initialized project

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+app.use(express.json()) 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// load .env 
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+// Session
+app.use(session
+    ({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true
+    }));
+
+// Routes (to be defined later)
+
+
+app.get('/', (req, res) => {
+    res.send("I love you baby");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
